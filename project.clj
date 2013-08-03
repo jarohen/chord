b2368deab5569de7f0fc2a593cece2c28448adf1
James Henderson <james@pcr.me.uk>
2013-08-03 12:59:44 +0100

    Adding core.async dep

--- project.clj
+++ project.clj
@@ -3,4 +3,10 @@
   :url "https://github.com/james-henderson/chord.git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+
+  ;; TODO when a core.async stable is released, remove this!
+  :repositories {"sonatype-oss-public"
+                 "https://oss.sonatype.org/content/groups/public/"}
+  
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.async "0.1.0-SNAPSHOT"]])
