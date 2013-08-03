cf0bfaf9f8c7bcb379ca4be6410c4391c3e40753
James Henderson <james@pcr.me.uk>
2013-08-03 12:56:11 +0100

    Turning a web-socket into a two way channel

---
+++ src/chord.cljs
@@ -0,0 +1,52 @@
+(ns chord
+  (:require [cljs.core.async :refer [chan <! >! put! close]]
+            [cljs.core.async.impl.protocols :as p])
+  (:require-macros [cljs.core.async.macros :refer (go)]))
+
+(defn make-read-ch [ws]
+  (let [ch (chan)]
+    (set! (.-onmessage ws)
+          (fn [ev]
+            (let [message (.-data ev)]
+              (put! ch {:message message}))))
+    ch))
+
+(defn make-write-ch [ws]
+  (let [ch (chan)]
+    (go
+     (loop []
+       (let [msg (<! ch)]
+         (.send ws msg))
+       (recur)))
+    ch))
+
+(defn make-open-ch [ws v]
+  (let [ch (chan)]
+    (set! (.-onopen ws)
+          (put! ch v))
+    ch))
+
+(defn combine-chans [read-ch write-ch]
+  (reify
+    p/ReadPort
+    (take! [_ handler]
+      (p/take! read-ch handler))
+
+    p/WritePort
+    (put! [_ msg handler]
+      (p/put! write-ch msg handler))
+
+    p/Channel
+    (close! [_]
+      (p/close! read-ch)
+      (p/close! write-ch))))
+
+(defn ws-ch [ws-url]
+  (let [web-socket (js/WebSocket. ws-url)
+        read-ch (make-read-ch web-socket)
+        write-ch (make-write-ch web-socket)
+        combined-ch (combine-chans read-ch write-ch)
+        socket-ch (make-open-ch web-socket combined-ch)]
+    socket-ch))
+
+
