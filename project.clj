070fa796505ef4ce163c830738a2d981174fb22c
James Henderson <james@pcr.me.uk>
2013-08-03 12:59:51 +0100

    Writing a quick description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject chord "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A library to bridge the gap between CLJS, web-sockets and core.async"
+  :url "https://github.com/james-henderson/chord.git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
