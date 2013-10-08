cadbd61d7bad06114ede459840f7ac338e01a03c
James Henderson <james@pcr.me.uk>
2013-10-08 13:41:29 +0100

    Really simple echo server and client - that logs to JS console

--- example-project/src/chord/example.clj
+++ example-project/src/chord/example.clj
@@ -1,10 +1,28 @@
 (ns chord.example
   (:require [ring.util.response :refer [response]]
             [compojure.core :refer [defroutes GET]]
-            [compojure.route :refer [resources]]))
+            [compojure.route :refer [resources]]
+            [chord.http-kit :refer [with-channel]]
+            [clojure.core.async :refer [<! >! put! close! go]]
+            [hiccup.page :refer [html5 include-js]]))
+
+(defn page-frame []
+  (html5
+   [:head
+    [:title "Chord Example"]
+    (include-js "/js/chord-example.js")]
+   [:body [:div "Hello World!"]]))
+
+(defn ws-handler [req]
+  (with-channel req ws
+    (go
+     (let [{:keys [message]} (<! ws)]
+       (println "Message received:" message)
+       (>! ws (format "You said: '%s'" message))
+       (Thread/sleep 500)
+       (close! ws)))))
 
 (defroutes app
-  (GET "/" [] (response "Hello world!"))
+  (GET "/" [] (response (page-frame)))
+  (GET "/ws" [] ws-handler)
   (resources "/js" {:root "js"}))
-
-(clojure.java.io/resource "js/chord-example.js")
--- example-project/src/chord/example_client.cljs
+++ example-project/src/chord/example_client.cljs
@@ -1,1 +1,13 @@
-(ns chord.example-client)
+(ns chord.example-client
+  (:require [chord.client :refer [ws-ch]]
+            [cljs.core.async :refer [<! >! put! close!]])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
+
+(go
+ (let [ws (<! (ws-ch "ws://localhost:3000/ws"))]
+   (>! ws "Hello server from client!")
+   (js/console.log (pr-str (<! ws)))))
+
+
+
+
