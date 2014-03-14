ca4e46a0fb3576750531cff44610c088f02f3b32
James Henderson <james@jarohen.me.uk>
2014-03-14 08:51:54 +0000

    Updating deps in example project

--- example-project/project.clj
+++ example-project/project.clj
@@ -4,20 +4,20 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [jarohen/chord "0.3.0"]
+                 [jarohen/chord "0.3.1-rc2"]
 
                  [ring/ring-core "1.2.0"]
                  [compojure "1.1.5"]
                  [hiccup "1.0.4"]
 
-                 [org.clojure/clojurescript "0.0-1913"]
-                 [org.clojure/tools.reader "0.7.8"]
+                 [org.clojure/clojurescript "0.0-2014"]
+                 [org.clojure/tools.reader "0.8.3"]
 
                  [prismatic/dommy "0.1.2"]]
 
   :plugins [[lein-pdo "0.1.1"]
-            [jarohen/lein-frodo "0.2.0"]
-            [lein-cljsbuild "0.3.3"]]
+            [jarohen/lein-frodo "0.2.11"]
+            [lein-cljsbuild "1.0.2"]]
 
   :frodo/config-resource "chord-example.edn"
 
