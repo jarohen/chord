0bd8d40d4c99b0226bbd4f15b9d555270063349d
James Henderson <james@jarohen.me.uk>
2014-05-04 18:25:12 +0100

    Bumping to 0.4.0-SNAPSHOT, and updating a couple of deps

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject jarohen/chord "0.3.1"
+(defproject jarohen/chord "0.4.0-SNAPSHOT"
   :description "A library to bridge the gap between CLJ/CLJS, web-sockets and core.async"
   :url "https://github.com/james-henderson/chord.git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [http-kit "2.1.10"]
-                 [org.clojure/tools.reader "0.8.3"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.async "0.1.301.0-deb34a-alpha"]
+                 [http-kit "2.1.18"]
+                 [org.clojure/tools.reader "0.8.4"]
                  [cheshire "5.3.1"]])
