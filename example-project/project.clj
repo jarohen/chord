e14b0f596fd6bc5d259d145f49e3ae834e0e9bba
James Henderson <james@jarohen.me.uk>
2014-05-05 12:45:58 +0100

    Fixing deps - downgrading to tools.reader 0.8.3 as per #16

--- example-project/project.clj
+++ example-project/project.clj
@@ -3,14 +3,14 @@
   :url "https://github.com/james-henderson/chord/example-project"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [jarohen/chord "0.3.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [jarohen/chord "0.4.0-SNAPSHOT"]
 
                  [ring/ring-core "1.2.0"]
                  [compojure "1.1.5"]
                  [hiccup "1.0.4"]
 
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [org.clojure/core.async "0.1.301.0-deb34a-alpha"]
                  [org.clojure/clojurescript "0.0-2202"]
 
                  [prismatic/dommy "0.1.2"]
