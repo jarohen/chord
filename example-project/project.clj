f22e2c2ace27dcb637acb8d53061a47726399342
James Henderson <james@jarohen.me.uk>
2014-05-05 21:50:03 +0100

    Bumping to 0.4.0 stable

--- example-project/project.clj
+++ example-project/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [jarohen/chord "0.4.0-rc1"]
+                 [jarohen/chord "0.4.0"]
 
                  [ring/ring-core "1.2.0"]
                  [compojure "1.1.5"]
