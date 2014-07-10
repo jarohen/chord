1a073ee8f720a1b7707af72dd574c3206761a4c3
James Henderson <james@jarohen.me.uk>
2014-07-10 19:04:24 +0100

    Bumping to SNAPSHOTs

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jarohen/chord "0.4.1"
+(defproject jarohen/chord "0.4.2-SNAPSHOT"
   :description "A library to bridge the gap between CLJ/CLJS, web-sockets and core.async"
   :url "https://github.com/james-henderson/chord.git"
   :license {:name "Eclipse Public License"
