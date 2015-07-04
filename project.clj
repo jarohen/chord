aafec5f2d600bf80ed32db5fcf79085ad835ad1e
James Henderson <james@jarohen.me.uk>
2015-07-04 17:45:47 +0100

    Dep upgrades

--- project.clj
+++ project.clj
@@ -6,14 +6,14 @@
 
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [http-kit "2.1.18"]
+                 [http-kit "2.1.19"]
                  [org.clojure/tools.reader "0.9.2"]
 
                  [com.cemerick/url "0.1.1"]
-                 [cheshire "5.3.1"]
+                 [cheshire "5.5.0"]
 
-                 [com.cognitect/transit-clj "0.8.259"]
-                 [com.cognitect/transit-cljs "0.8.192"]
+                 [com.cognitect/transit-clj "0.8.275"]
+                 [com.cognitect/transit-cljs "0.8.220"]
 
                  [org.clojure/data.fressian "0.2.0"]
                  [net.unit8/fressian-cljs "0.1.0"]])
