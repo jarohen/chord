b736df8bafb70c5f3c9a3cfc480c510a44bb8e52
Thomas Getgood <thomas.getgood@gmail.com>
2017-09-21 11:29:35 -0400

    Removed redundant comments.
    They're in the README now.

--- src/chord/node.cljs
+++ src/chord/node.cljs
@@ -12,32 +12,3 @@
                (handler ws-ch request))))
       server)
     (throw (js/Error. "Failed to load ws (nodejs websocket library). Make sure it's installed."))))
-
-(comment
-  "Plain example"
-
-  (defn handler [ws-ch req]
-    (if (authorized? req)
-      (async/put! ws-ch "Welcome!")
-      (do
-        (async/put! ws-ch "Denied!")
-        (async/close! ws-ch)))
-    (async/go
-      (println (async/<! ws-ch))
-      '...))
-
-  (def server (chord.node/ws-server {:port 8080} {:format :edn}) handler)
-
-  "Express Example"
-
-  (def express (nodejs/require "express"))
-  (def https (nodejs/require "https"))
-  (def app (express))
-  '...
-
-  ;; Note: SSL is inherited from the webserver, so using http gives you ws, and
-  ;; https gives you wss.
-  (def server (.createServer https app))
-
-  (def ws-server
-    (chord.node/ws-server server {:format :json} handler)))
