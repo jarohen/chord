5304960de28fa6c9359175dbce9d13016158bab3
James Henderson <james@jarohen.me.uk>
2017-10-03 13:16:23 +0100

    Documenting chord.node/ws-server

--- src/chord/node.cljs
+++ src/chord/node.cljs
@@ -3,7 +3,18 @@
             [chord.channels :as channels]
             [chord.format :as format]))
 
-(defn ws-server [handler {:keys [read-ch write-ch ws-lib-opts] :as opts}]
+(defn ws-server
+  "Arguments:
+    handler: a 2-arg fn accepting the ws-channel and details of the request
+    opts:
+     - read-ch         - channel to use for reading the websocket
+     - write-ch        - channel to use for writing to the websocket
+     - format          - data format to use on the channel
+                         either :edn (default), :json, :json-kw, :transit-json, or :str.
+     - ws-lib-opts     - options to pass to 'ws', the underlying node library.
+                         see https://github.com/websockets/ws for more details"
+  [handler {:keys [read-ch write-ch ws-lib-opts] :as opts}]
+
   (if-let [ws (nodejs/require "ws")]
     (let [server (ws.Server. (clj->js ws-lib-opts))]
       (.on server "connection"
