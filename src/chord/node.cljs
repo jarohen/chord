99c25386ca189c9d17ca44d5fe8d692019d890d7
James Henderson <james@jarohen.me.uk>
2017-10-03 13:02:42 +0100

    Prefer to keep one options map - bringing ws-opts into the main ws-server opts map

--- src/chord/node.cljs
+++ src/chord/node.cljs
@@ -3,12 +3,13 @@
             [chord.channels :as channels]
             [chord.format :as format]))
 
-(defn ws-server [handler {:keys [read-ch write-ch] :as chord-opts} ws-opts]
+(defn ws-server [handler {:keys [read-ch write-ch ws-opts] :as opts}]
   (if-let [ws (nodejs/require "ws")]
     (let [server (ws.Server. (clj->js ws-opts))]
       (.on server "connection"
            (fn [socket request]
-             (let [ws-ch (channels/wrap-websocket socket chord-opts)]
+             (let [ws-ch (channels/wrap-websocket socket opts)]
                (handler ws-ch request))))
       server)
+
     (throw (js/Error. "Failed to load ws (nodejs websocket library). Make sure it's installed."))))
